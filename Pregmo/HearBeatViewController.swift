43385bb9295d6bab71ae9e1038150d6d6a4d0e7d
Deyan Zhekov <deyan_zhekov@yahoo.com>
2017-03-19 11:06:16 +0200

    Added new view controllers for the tab bar.

--- Pregmo/HearBeatViewController.swift
+++ Pregmo/HearBeatViewController.swift
@@ -1,5 +1,5 @@
 //
-//  SecondViewController.swift
+//  HearBeatViewController.swift
 //  Pregmo
 //
 //  Created by Deyan Zhekov on 3/19/17.
@@ -8,18 +8,18 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class HearBeatViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 
 }
-
